b7f5cf88eb669bb1f270b61cc86b842f2bde8859
Viktor Back <viktor@back.tm>
2025-09-24 13:28:31 +0200

    solvar för x,y,vx,vy nu

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
-
 # m(t) ̄a(t) =  ̄F + m′(t) ̄u(t)
 # ̄a(t) =  ̄(F + m′(t) ̄u(t))/m(t)
 
@@ -10,16 +9,18 @@ from scipy.integrate import solve_ivp
 # ̄v'(t) =  (m(t) ̄g − c|| ̄v(t)|| ̄v(t) + m′(t) ̄u(t))/m(t)
 # v(0) = 0
 
-
-h = 0.1
+h = 1
 t0 = 0
-t1 = 30
+t1 = 15
 t_span = (t0, t1)
 tt = np.arange(t0, t1, h)
-v0 = np.array([0,0])
+
+# x,y,vx,vy
+v0 = np.array([0,0,0,0])
 c = 0.05 # kg/m
 k_m = 700 # m/s
 position_0 = np.array([0, 0]) # x och y
+positions = np.zeros_like(position_0)
 goal_cords = np.array([80, 60]) # mål koordinator
 t = 0
 t_max = 10
@@ -39,53 +40,62 @@ def m_prim(t):
         return 0
     
 # Det vi kommer styra med
-def angle(t):
-    return -np.pi / 2
+def angle(t, pos):
+    dx = goal_cords[0] - pos[0]
+    dy = goal_cords[1] - pos[1]
+    return np.arctan2(dy,dx)
+    return np.pi/2
+    if(pos[1]>=20):
+        dx = goal_cords[0] - pos[0]
+        dy = goal_cords[1] - pos[1]
+        print(dx)
+        print(dy)
+        return np.arctan2(dy,dx)
+    else:
+        return np.pi/2
      
 
 # Funktioner för hastighetsvektorn
-def u_x(t):
-    return k_m * np.cos(angle(t))
+def u_x(t,ang):
+    return k_m * np.cos(ang)
 
-def u_y(t):
-    return k_m * np.sin(angle(t))
-
+def u_y(t,ang):
+    return k_m * np.sin(ang)
 
 #hastighetsvektor för bränslet
-def u(t):
-    return np.array([u_x(t), u_y(t)])
+def u(t,pos):
+    ang = angle(t,pos)
+    return np.array([u_x(t,ang), u_y(t,ang)])
 
 # Alla externa krafter som verkar på raketen
 def F_vector(t, v):
     return m(t) * g - c * np.linalg.norm(v) * v
 # f(t, v) = (F + m′(t) ̄u(t))/m(t)
 
-
 # ̄v'(t) =  (F + m′(t) * u(t))/m(t)
 # v(0) = 0
 def ode_rhs(t, v):
     #return (m(t) ̄g − c|| ̄v(t)|| ̄v(t) + m′(t) ̄u(t))/m(t)
-    F1, F2 = F_vector(t, v)
-    print("F1 = " + str(F1) + ", F2 = " + str(F2))
-
-    u1, u2 = u(t)
-    return np.array([((F1 + m_prim(t)) * u1)/ m(t), (F2 + m_prim(t) * u2) / m(t)])
-    #return (F_vector(t) + m_prim(t) * u(t)) / m(t)
-
+    x,y,vx,vy = v
+    pos = np.array([x,y])
+    v_tot = np.array([vx,vy])
+    y = (F_vector(t,v_tot + m_prim(t) * u(t,pos))/m(t))
+    return np.array([vx,vy, y[0], y[1]])
 
 sol = solve_ivp(ode_rhs, t_span, v0, t_eval = tt)
 
-y_pos = np.zeros(int(t1 / h))
-x_pos = np.zeros(int(t1 / h))
-for i in range(len(tt) - 1):
-    x_pos[i + 1] = x_pos[i] + sol.y[0][i] * 0.1
-    y_pos[i + 1] = y_pos[i] + sol.y[1][i] * 0.1
+#y_pos = np.zeros(int(t1 / h))
+#x_pos = np.zeros(int(t1 / h))
+#for i in range(len(tt) - 1):
+#    x_pos[i + 1] = x_pos[i] + sol.y[2][i] * 0.1
+#    y_pos[i + 1] = y_pos[i] + sol.y[3][i] * 0.1
 
-
-plt.plot(sol.t, sol.y[0], label='v_x')
-plt.plot(sol.t, sol.y[1], label='v_y')
-plt.plot(sol.t, y_pos, label='y')
-plt.plot(sol.t, x_pos, label='x')
+plt.plot(sol.t, sol.y[0], label='x')
+plt.plot(sol.t, sol.y[1], label='y')
+plt.plot(sol.t, sol.y[2], label='v_x')
+plt.plot(sol.t, sol.y[3], label='v_y')
+#plt.plot(sol.t, y_pos, label='y')
+#plt.plot(sol.t, x_pos, label='x')
 plt.title('Uppgift 2')
 plt.xlabel('tid, t')
 plt.ylabel('v')
