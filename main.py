07ef9bcbeddf2bf5048871941a2d02e5b8fc12cc
Viktor Back <viktor@back.tm>
2025-09-24 13:49:53 +0200

    dum algoritm

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from scipy.integrate import solve_ivp
 # ̄v'(t) =  (m(t) ̄g − c|| ̄v(t)|| ̄v(t) + m′(t) ̄u(t))/m(t)
 # v(0) = 0
 
-h = 1
+h = 0.01
 t0 = 0
 t1 = 15
 t_span = (t0, t1)
@@ -19,8 +19,6 @@ tt = np.arange(t0, t1, h)
 v0 = np.array([0,0,0,0])
 c = 0.05 # kg/m
 k_m = 700 # m/s
-position_0 = np.array([0, 0]) # x och y
-positions = np.zeros_like(position_0)
 goal_cords = np.array([80, 60]) # mål koordinator
 t = 0
 t_max = 10
@@ -41,18 +39,12 @@ def m_prim(t):
     
 # Det vi kommer styra med
 def angle(t, pos):
-    dx = goal_cords[0] - pos[0]
-    dy = goal_cords[1] - pos[1]
-    return np.arctan2(dy,dx)
-    return np.pi/2
-    if(pos[1]>=20):
-        dx = goal_cords[0] - pos[0]
+    if pos[1] >= 20:
+        dx = pos[0] - goal_cords[0]
         dy = goal_cords[1] - pos[1]
-        print(dx)
-        print(dy)
-        return np.arctan2(dy,dx)
+        return -np.arctan2(dy, dx)
     else:
-        return np.pi/2
+        return -np.pi / 2  
      
 
 # Funktioner för hastighetsvektorn
@@ -76,11 +68,11 @@ def F_vector(t, v):
 # v(0) = 0
 def ode_rhs(t, v):
     #return (m(t) ̄g − c|| ̄v(t)|| ̄v(t) + m′(t) ̄u(t))/m(t)
-    x,y,vx,vy = v
-    pos = np.array([x,y])
-    v_tot = np.array([vx,vy])
-    y = (F_vector(t,v_tot + m_prim(t) * u(t,pos))/m(t))
-    return np.array([vx,vy, y[0], y[1]])
+    x, y, vx, vy = v
+    pos = np.array([x, y])
+    v_tot = np.array([vx, vy])
+    a = (F_vector(t, v_tot) + m_prim(t) * u(t,pos))/m(t)
+    return np.array([vx,vy, a[0], a[1]])
 
 sol = solve_ivp(ode_rhs, t_span, v0, t_eval = tt)
 
@@ -90,15 +82,25 @@ sol = solve_ivp(ode_rhs, t_span, v0, t_eval = tt)
 #    x_pos[i + 1] = x_pos[i] + sol.y[2][i] * 0.1
 #    y_pos[i + 1] = y_pos[i] + sol.y[3][i] * 0.1
 
-plt.plot(sol.t, sol.y[0], label='x')
-plt.plot(sol.t, sol.y[1], label='y')
-plt.plot(sol.t, sol.y[2], label='v_x')
-plt.plot(sol.t, sol.y[3], label='v_y')
-#plt.plot(sol.t, y_pos, label='y')
-#plt.plot(sol.t, x_pos, label='x')
-plt.title('Uppgift 2')
-plt.xlabel('tid, t')
-plt.ylabel('v')
+plt.figure(figsize=(10,5))
+
+plt.subplot(1,2,1)
+plt.plot(sol.y[0], sol.y[1], label='Trajectory')
+plt.scatter(goal_cords[0],goal_cords[1], color='red', label='Goal')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Raket')
 plt.legend()
 plt.grid()
+
+plt.subplot(1,2,2)
+plt.plot(sol.t, sol.y[2], label='v_x')
+plt.plot(sol.t, sol.y[3], label='v_y')
+plt.xlabel('Tid')
+plt.ylabel('Hastighet')
+plt.title('Raket hastigheter')
+plt.legend()
+plt.grid()
+
+plt.tight_layout()
 plt.show()
