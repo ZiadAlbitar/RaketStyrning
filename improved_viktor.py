9188fca69e4a254fe80f72f02d4e964c626ba71f
Ziad Albitar <ziad@Ziads-MacBook-Air.local>
2025-10-02 12:07:10 +0200

    Cleaned up merge

--- improved_viktor.py
+++ improved_viktor.py
@@ -1,9 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from common import goal_cords, F_vector
+import time
 
+start_time = time.time()
 c = 0.05 # kg/m
 k_m = 700 # m/s
 goal_cords = np.array([60, 80])
 g = np.array([0, -9.82]) # Vektor gravitation
+
+# x,y,vx,vy
+v0 = np.array([0,0,0,0])
+
+h = 0.01
+t0 = 0
+t1 = 10
+t_span = (t0, t1)
+tt = np.arange(t0, t1, h)
 
 
 # massfunktion
@@ -75,10 +89,6 @@ def u(pos, v):
     ang = angle(pos, v)
     return np.array([u_x(ang), u_y(ang)])
 
-# Alla externa krafter som verkar på raketen
-def F_vector(t, v):
-    return m(t) * g - c * np.linalg.norm(v) * v
-
 def ode_rhs(t, v):
     x, y, vx, vy = v
     pos = np.array([x, y])
@@ -86,3 +96,29 @@ def ode_rhs(t, v):
     a = (F_vector(t, v_tot) + m_prim(t) * u(pos, v_tot))/m(t)
     # Returnera en array med hastigheten och accelration i x- och y-led
     return np.array([vx, vy, a[0], a[1]])
+
+sol = solve_ivp(ode_rhs, t_span, v0, t_eval = tt)
+run_time = str(time.time() - start_time)
+print("Run time: " + run_time)
+
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+plt.plot(sol.y[0], sol.y[1], label='Trajectory')
+plt.scatter(goal_cords[0],goal_cords[1], color='red', label='Goal')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Raket')
+plt.legend()
+plt.grid()
+
+plt.subplot(1,2,2)
+plt.plot(sol.t, sol.y[2], label='v_x')
+plt.plot(sol.t, sol.y[3], label='v_y')
+plt.xlabel('Tid')
+plt.ylabel('Hastighet')
+plt.title('Raket hastigheter')
+plt.legend()
+plt.grid()
+
+plt.tight_layout()
+plt.show()
